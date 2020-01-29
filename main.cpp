c5112b16c890e3bf42ad81c9ba05df698cd07ac3
lcnorine <lcnorine@mail.csuchico.edu>
2020-01-29 11:34:55 -0800

    lcnorine

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@ using std::cout;
 int main()
 {
     cout<<"Hi, please enter two numbers: ";
-
+    int n1;
+    int n2;
+    cin >> n1;
+    cin >> n2;
+    cout << "I find it interesting that you have " << n1 << " eyeballs and " << n2 << " fingernails in your sock drawer.\n";
     return 0;
 }
